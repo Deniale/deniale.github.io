d2855ad226ab8371a8aa0b3bd5e695e3f747a392
Deniale <33962484+Deniale@users.noreply.github.com>
2017-12-07 22:21:44 +0200

    Add files via upload

---
+++ doc4/js/custom.js
@@ -0,0 +1,36 @@
+$(function () {
+	// $('.works-slider').slick({
+	// 	arrows:false,
+	// 	dots:true,
+	// 	autoplay:true,
+	// 	autoplaySpeed:4000
+	// })
+	 $('.cool-slider').slick({
+  		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+	 	autoplay:true,
+	});
+	// $('.works-slider').slick({
+	// 	slidesToShow: 5,
+	// 	centerPadding: '10px',
+	// 	slidesToScroll: 1,
+	// 	asNavFor: '.slider-for',
+	// 	dots: false,
+	// 	centerMode: true,
+	// 	focusOnSelect: true,
+	// 	nextArrow: '<i class="fa fa-arrow-right"></i>',
+ //  		prevArrow: '<i class="fa fa-arrow-left"></i>'
+	// });
+});
+
+// $(function () {
+//     $('.works-slider').slick({
+//       infinite: true,
+//       slidesToShow: 3,
+//       slidesToScroll: 3
+//     });
+// });
+
